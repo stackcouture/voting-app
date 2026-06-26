c4a023fa1b6f51f6a26bdcbffd6495c4a520210e
Naveen <naveenramlu28@gmail.com>
2026-06-26 22:02:43 +0530

    #Update the app.js in the result

--- result/views/app.js
+++ result/views/app.js
@@ -25,3 +25,17 @@ app.controller('statsCtrl', function ($scope) {
     });
 
 });
+
+function getPercentages(a, b) {
+    var result = {};
+
+    if (a + b > 0) {
+        result.a = Math.round(a / (a + b) * 100);
+        result.b = 100 - result.a;
+    } else {
+        result.a = 50;
+        result.b = 50;
+    }
+
+    return result;
+}
